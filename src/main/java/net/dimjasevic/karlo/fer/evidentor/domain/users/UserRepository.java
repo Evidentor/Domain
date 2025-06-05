c1a45255ac440cb9cfd06c11afc528122c63198e
Karlito16 <karlo.dimjasevic@gmail.com>
2025-06-05 22:12:14 +0200

    Initial grpc setup for decision service

--- src/main/java/net/dimjasevic/karlo/fer/evidentor/domain/users/UserRepository.java
+++ src/main/java/net/dimjasevic/karlo/fer/evidentor/domain/users/UserRepository.java
@@ -3,6 +3,10 @@ package net.dimjasevic.karlo.fer.evidentor.domain.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByCardId(String cardId);
 }
