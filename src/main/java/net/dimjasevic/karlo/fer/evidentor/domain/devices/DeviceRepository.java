4ae9f374dd371eabf8f25d563d2f5bf63a10bd1c
Karlito16 <karlo.dimjasevic@gmail.com>
2025-06-11 13:22:53 +0200

    OpenAPI lib and get device by serial num lib [build]

--- src/main/java/net/dimjasevic/karlo/fer/evidentor/domain/devices/DeviceRepository.java
+++ src/main/java/net/dimjasevic/karlo/fer/evidentor/domain/devices/DeviceRepository.java
@@ -3,6 +3,10 @@ package net.dimjasevic.karlo.fer.evidentor.domain.devices;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+    Optional<Device> getBySerialNumber(String serialNumber);
 }
